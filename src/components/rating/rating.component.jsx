cdacae172d60a535777729fa1a5589d7d2143c44
Benjamin Cormier <benjamin.cormier@outlook.com>
2023-06-27 17:05:09 +0200

    improved Rating component

--- src/components/rating/rating.component.jsx
+++ src/components/rating/rating.component.jsx
@@ -4,31 +4,25 @@ import starInactive from "../../assets/star-inactive.svg";
 import "./rating.styles.scss";
 
 const Rating = ({ stars }) => {
-  const numberOfStars = +stars; // string to number
   const totalStars = 5;
-  const activeStars = numberOfStars;
+  const activeStars = +stars; // string to number
   const inactiveStars = totalStars - activeStars;
+
+  const starsArray = Array(activeStars)
+    .fill("*")
+    .concat(Array(inactiveStars).fill("-"));
+
+  // starsArray sera de la forme : ['*', '*', '*', '-', '-']
 
   return (
     <div className="rating">
-      {Array(activeStars)
-        .fill(undefined)
-        .map((_, index) => (
-          <img
-            key={`active-star-${index}`}
-            src={starActive}
-            alt="active star"
-          />
-        ))}
-      {Array(inactiveStars)
-        .fill(undefined)
-        .map((_, index) => (
-          <img
-            key={`inactive-star-${index}`}
-            src={starInactive}
-            alt="inactive star"
-          />
-        ))}
+      {starsArray.map((item, index) =>
+        item === "*" ? (
+          <img key={index} src={starActive} alt="active star"></img>
+        ) : (
+          <img key={index} src={starInactive} alt="inactive star"></img>
+        )
+      )}
     </div>
   );
 };
